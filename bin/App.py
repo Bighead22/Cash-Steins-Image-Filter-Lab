2f219854865d4db75e840417cbc963736461a41a
Bighead22 <cashstein1@icloud.com>
2026-08-18 22:42:49 -0700

    csseProject1

---
+++ bin/App.py
@@ -0,0 +1,14 @@
+from cmu_graphics import *
+
+print("Hello, World!")
+
+#run from terminal: python src/App.py
+#put pip install cmu-graphics in terminal if you don't have it yet
+
+#paste pixle art under here
+#
+
+
+
+#
+cmu_graphics.run()
